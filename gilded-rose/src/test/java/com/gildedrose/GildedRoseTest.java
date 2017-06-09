ca9910aab0b49c3d16dd8b9f93d011e2a781f14c
Juan Ramírez Ruiz <juan.ramirez.ruiz@gmail.com>
2017-06-09 11:44:39 +0200

    Added test to check that the quality of a generic item doesn't become negative.

--- gilded-rose/src/test/java/com/gildedrose/GildedRoseTest.java
+++ gilded-rose/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -26,4 +26,13 @@ public class GildedRoseTest {
         assertEquals(0, app.items[0].quality);
     }
 
+    @Test
+    public void thatTheQualityOfAGenericItemDoesNotBecomeNegative() {
+        Item[] items = new Item[] { new Item("foo", 0, 0) };
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+
+        assertEquals(0, app.items[0].quality);
+    }
 }
