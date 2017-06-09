abc6b07980e343352124ab0ec99d365f78901148
Juan Ramírez Ruiz <juan.ramirez.ruiz@gmail.com>
2017-06-09 11:38:43 +0200

    Added test to check that a generic item degrades quality value when being greater than zero.

--- gilded-rose/src/test/java/com/gildedrose/GildedRoseTest.java
+++ gilded-rose/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -16,4 +16,14 @@ public class GildedRoseTest {
         assertEquals(0, app.items[0].sellIn);
     }
 
+    @Test
+    public void thatAGenericItemDegradesQualityValueWhenBeingGreaterThanZero() {
+        Item[] items = new Item[] { new Item("foo", 0, 1) };
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+
+        assertEquals(0, app.items[0].quality);
+    }
+
 }
