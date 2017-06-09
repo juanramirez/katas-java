aa08deb1cf2ad88525a12b74e520dabbfb117500
Juan Ramírez Ruiz <juan.ramirez.ruiz@gmail.com>
2017-06-09 14:13:34 +0200

    Added test to check that Sulfuras does not change its sellIn value.

--- gilded-rose/src/test/java/com/gildedrose/GildedRoseTest.java
+++ gilded-rose/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -65,4 +65,14 @@ public class GildedRoseTest {
 
         assertEquals(50, app.items[0].quality);
     }
+
+    @Test
+    public void thatSulfurasDoesNotChangeItsSellInValue() {
+        Item[] items = new Item[] { new Item("Sulfuras, Hand of Ragnaros", 1, 1) };
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+
+        assertEquals(1, app.items[0].sellIn);
+    }
 }
