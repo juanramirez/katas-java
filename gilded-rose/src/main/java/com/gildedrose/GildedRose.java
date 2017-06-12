455a20dc4fc6dd76d8b8407298b97659940c6310
Juan Ramírez Ruiz <juan.ramirez.ruiz@gmail.com>
2017-06-12 18:04:56 +0200

    Extracted method to increase non legendary item quality.

--- gilded-rose/src/main/java/com/gildedrose/GildedRose.java
+++ gilded-rose/src/main/java/com/gildedrose/GildedRose.java
@@ -22,15 +22,11 @@ class GildedRose {
 
 					if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
 						if (items[i].sellIn < 11) {
-							if (items[i].quality < 50) {
-								items[i].quality = items[i].quality + 1;
-							}
+							increaseNonLegendaryItemQuality(items[i]);
 						}
 
 						if (items[i].sellIn < 6) {
-							if (items[i].quality < 50) {
-								items[i].quality = items[i].quality + 1;
-							}
+							increaseNonLegendaryItemQuality(items[i]);
 						}
 					}
 				}
@@ -52,11 +48,15 @@ class GildedRose {
 						items[i].quality = 0;
 					}
 				} else {
-					if (items[i].quality < 50) {
-						items[i].quality = items[i].quality + 1;
-					}
+					increaseNonLegendaryItemQuality(items[i]);
 				}
 			}
+		}
+	}
+
+	private void increaseNonLegendaryItemQuality(Item item) {
+		if (item.quality < 50) {
+			item.quality = item.quality + 1;
 		}
 	}
 }
