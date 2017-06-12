4cf5e47a16cb4ae9e704b876f8632b08cb2ea873
Juan Ramírez Ruiz <juan.ramirez.ruiz@gmail.com>
2017-06-12 18:18:58 +0200

    Extracted method to drop the quality of a generic item.

--- gilded-rose/src/main/java/com/gildedrose/GildedRose.java
+++ gilded-rose/src/main/java/com/gildedrose/GildedRose.java
@@ -11,10 +11,8 @@ class GildedRose {
 		for (int i = 0; i < items.length; i++) {
 			if (!items[i].name.equals("Aged Brie")
 					&& !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-				if (items[i].quality > 0) {
-					if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-						items[i].quality = items[i].quality - 1;
-					}
+				if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+					dropGenericItemQuality(items[i]);
 				}
 			} else {
 				if (items[i].quality < 50) {
@@ -39,10 +37,8 @@ class GildedRose {
 			if (items[i].sellIn < 0) {
 				if (!items[i].name.equals("Aged Brie")) {
 					if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-						if (items[i].quality > 0) {
-							if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-								items[i].quality = items[i].quality - 1;
-							}
+						if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+							dropGenericItemQuality(items[i]);
 						}
 					} else {
 						items[i].quality = 0;
@@ -51,6 +47,12 @@ class GildedRose {
 					increaseNonLegendaryItemQuality(items[i]);
 				}
 			}
+		}
+	}
+
+	private void dropGenericItemQuality(Item item) {
+		if (item.quality > 0) {
+			item.quality = item.quality - 1;
 		}
 	}
 
