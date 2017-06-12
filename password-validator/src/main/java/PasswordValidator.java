a0c300cdffa0079d7dd562dd5077861592e031f9
Juan Ramírez Ruiz <juan.ramirez.ruiz@gmail.com>
2017-06-12 19:40:57 +0200

    Fixed latest test.

--- password-validator/src/main/java/PasswordValidator.java
+++ password-validator/src/main/java/PasswordValidator.java
@@ -5,8 +5,11 @@ class PasswordValidator {
 
     static boolean check(String password) {
         Pattern capitalLettersPattern = Pattern.compile("[A-Z]");
-        Matcher capitalLettersMatcher = capitalLettersPattern.matcher(password);
+        Pattern numbersPattern = Pattern.compile("[0-9]");
 
-        return password.length() > 8 && capitalLettersMatcher.find();
+        Matcher capitalLettersMatcher = capitalLettersPattern.matcher(password);
+        Matcher numbersMatcher = numbersPattern.matcher(password);
+
+        return password.length() > 8 && capitalLettersMatcher.find() && numbersMatcher.find();
     }
 }
