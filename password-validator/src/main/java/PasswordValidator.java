027c9e9b01f6d8688401f39ec32544decf5d0ad9
Juan Ramírez Ruiz <juan.ramirez.ruiz@gmail.com>
2017-06-12 19:51:22 +0200

    Fixed latest test.

--- password-validator/src/main/java/PasswordValidator.java
+++ password-validator/src/main/java/PasswordValidator.java
@@ -7,14 +7,17 @@ class PasswordValidator {
         Pattern capitalLettersPattern = Pattern.compile("[A-Z]");
         Pattern lowercaseLettersPattern = Pattern.compile("[a-z]");
         Pattern numbersPattern = Pattern.compile("[0-9]");
+        Pattern underscorePattern = Pattern.compile("_");
 
         Matcher capitalLettersMatcher = capitalLettersPattern.matcher(password);
         Matcher lowercaseLettersMatcher = lowercaseLettersPattern.matcher(password);
         Matcher numbersMatcher = numbersPattern.matcher(password);
+        Matcher underscoreMatcher = underscorePattern.matcher(password);
 
         return password.length() > 8
                 && capitalLettersMatcher.find()
                 && lowercaseLettersMatcher.find()
-                && numbersMatcher.find();
+                && numbersMatcher.find()
+                && underscoreMatcher.find();
     }
 }
