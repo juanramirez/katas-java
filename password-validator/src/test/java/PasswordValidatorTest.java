06d4ba3567cec274f6b31c522cfcc16fbe57cf4b
Juan Ramírez Ruiz <juan.ramirez.ruiz@gmail.com>
2017-06-13 20:54:08 +0200

    Made test thatValidatorReturnsFalseWhenPasswordHas8Characters more specific.

--- password-validator/src/test/java/PasswordValidatorTest.java
+++ password-validator/src/test/java/PasswordValidatorTest.java
@@ -6,15 +6,15 @@ import static junit.framework.TestCase.assertTrue;
 public class PasswordValidatorTest {
 
 	@Test
-	public void thatValidatorReturnsFalseWhenPasswordHas8Characters() {
-		String password = "12345678";
-		assertFalse(PasswordValidator.check(password));
-	}
-
-	@Test
 	public void thatValidatorReturnsTrueWhenAValidPasswordIsGiven() {
 		String password = "ab1Y_ph4T";
 		assertTrue(PasswordValidator.check(password));
+	}
+
+	@Test
+	public void thatValidatorReturnsFalseWhenPasswordHas8Characters() {
+		String password = "a1Y_ph4T";
+		assertFalse(PasswordValidator.check(password));
 	}
 
 	@Test
