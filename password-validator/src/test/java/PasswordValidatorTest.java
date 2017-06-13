9ba8841ca56f58d943c85eea87f8af730b12e016
Juan Ramírez Ruiz <juan.ramirez.ruiz@gmail.com>
2017-06-13 21:01:35 +0200

    More descriptive test names.

--- password-validator/src/test/java/PasswordValidatorTest.java
+++ password-validator/src/test/java/PasswordValidatorTest.java
@@ -4,39 +4,38 @@ import static junit.framework.TestCase.assertFalse;
 import static junit.framework.TestCase.assertTrue;
 
 public class PasswordValidatorTest {
-
 	@Test
-	public void thatValidatorReturnsTrueWhenAValidPasswordIsGiven() {
+	public void thatPasswordIsValidWhenAllRequirementsAreFulfilled() {
 		String password = "ab1Y_ph4T";
 		assertTrue(PasswordValidator.isValid(password));
 	}
 
 	@Test
-	public void thatValidatorReturnsFalseWhenPasswordHas8Characters() {
+	public void thatPasswordIsNotValidWhenAllRequirementsButLengthGreaterThan8AreFulfilled() {
 		String password = "a1Y_ph4T";
 		assertFalse(PasswordValidator.isValid(password));
 	}
 
 	@Test
-	public void thatValidatorReturnsFalseWithAPasswordWithoutCapitalLetters() {
+	public void thatPasswordIsNotValidWhenAllRequirementsButCapitalLettersAreFulfilled() {
 		String password = "ab1y_ph4t";
 		assertFalse(PasswordValidator.isValid(password));
 	}
 
     @Test
-    public void thatValidatorReturnsFalseWithAPasswordWithoutNumbers() {
+    public void thatPasswordIsNotValidWhenAllRequirementsButNumbersAreFulfilled() {
         String password = "abcY_phlT";
         assertFalse(PasswordValidator.isValid(password));
     }
 
 	@Test
-	public void thatValidatorReturnsFalseWithAPasswordWithoutLowercaseLetters() {
+	public void thatPasswordIsNotValidWhenAllRequirementsButLowercaseLettersAreFulfilled() {
 		String password = "AB1Y_PH4T";
 		assertFalse(PasswordValidator.isValid(password));
 	}
 
 	@Test
-	public void thatValidatorReturnsFalseWithAPasswordWithoutUnderscores() {
+	public void thatPasswordIsNotValidWhenAllRequirementsButUnderscoresAreFulfilled() {
 		String password = "ab1YJph4T";
 		assertFalse(PasswordValidator.isValid(password));
 	}
